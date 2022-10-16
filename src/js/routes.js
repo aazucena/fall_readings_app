fe1e25ce85b7b07daa35564760fede3a870d6df3
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-10-15 20:32:50 -0600

    added routes and removed the search function

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,13 +1,70 @@
+import IndexPage from '@/pages/index.f7'
+import HomePage from '@/pages/home/index.f7'
+import VideosPage from '@/pages/videos/index.f7'
+import VideosDetailsPage from '@/pages/videos/details.f7'
+import AudioPage from '@/pages/audio/index.f7'
+import AudioDetailsPage from '@/pages/audio/details.f7'
+import PhotosPage from '@/pages/photos/index.f7'
+import PhotosDetailsPage from '@/pages/photos/details.f7'
+import BooksPage from '@/pages/books/index.f7'
+import BooksDetailsPage from '@/pages/books/details.f7'
+import NotFoundPage from '@/pages/404.f7'
 
-import HomePage from '../pages/home.f7';
 
+const tabs = [
+  {
+    name: 'videos', 
+    components: { 
+      index: VideosPage, 
+      details: VideosDetailsPage 
+    }
+  },
+  {
+    name: 'audio', 
+    components: { 
+      index: AudioPage, 
+      details: AudioDetailsPage 
+    }
+  },
+  {
+    name: 'photos', 
+    components: { 
+      index: PhotosPage, 
+      details: PhotosDetailsPage 
+    }
+  },
+  {
+    name: 'books', 
+    components: { 
+      index: BooksPage, 
+      details: BooksDetailsPage 
+    }
+  }
+].map(({name, components}) => ({
+  path: `/${name}/`,
+  id: `${name}-tab`,
+  component: components.index,
+  routes: [
+    {
+      path: 'details/:id',
+      component: components.details,
+    }
+  ]
+}))
 
-import NotFoundPage from '../pages/404.f7';
-
+console.log("🚀 ~ file: routes.js ~ line 43 ~ tabs", tabs)
 var routes = [
   {
     path: '/',
-    component: HomePage,
+    component: IndexPage,
+    tabs: [
+      {
+        path: '/',
+        id: 'home-tab',
+        component: HomePage,
+      },
+      ...tabs
+    ],
   },
 
 
